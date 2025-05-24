44ffb4d67f742daf098724745acaf96b61414c23
shaxriyorweb <choliyevshaxriyor6@gmail.com>
2025-05-24 22:43:24 +0500

    init_db.py

--- init_db.py
+++ init_db.py
@@ -3,27 +3,24 @@ import sqlite3
 def init_db():
     conn = sqlite3.connect('users.db')
     c = conn.cursor()
-
-    # Jadval yaratish
+    # users jadvalini yaratamiz
     c.execute('''
-    CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        username TEXT UNIQUE NOT NULL,
-        password TEXT NOT NULL,
-        first_name TEXT NOT NULL,
-        last_name TEXT NOT NULL
-    )
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT UNIQUE NOT NULL,
+            password TEXT NOT NULL,
+            first_name TEXT NOT NULL,
+            last_name TEXT NOT NULL
+        )
     ''')
-
-    # Test foydalanuvchi qo'shish (parol oddiy matn: 12345)
+    # Test foydalanuvchi qo'shamiz (login=user1, parol=12345)
     c.execute('''
-    INSERT OR IGNORE INTO users (username, password, first_name, last_name)
-    VALUES (?, ?, ?, ?)
+        INSERT OR IGNORE INTO users (username, password, first_name, last_name)
+        VALUES (?, ?, ?, ?)
     ''', ('user1', '12345', 'Ali', 'Valiyev'))
-
     conn.commit()
     conn.close()
-    print("Bazani yaratish va test foydalanuvchi qo'shish yakunlandi.")
+    print("Bazani yaratish va test foydalanuvchi qo'shish tugadi.")
 
 if __name__ == "__main__":
     init_db()
